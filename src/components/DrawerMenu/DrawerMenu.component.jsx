e2499d98603a5155e21f2b664d8ed63dd2ebb2bf
Ricardo Leonel Acosta <ricardo.acosta@ricardo.acosta>
2020-12-01 16:47:00 -0600

    Creates navbar

---
+++ src/components/DrawerMenu/DrawerMenu.component.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import HomeIcon from '@material-ui/icons/Home';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Liked Videos', 'Home'].map((text, index) => (
+            <ListItem button key={text}>
+                <ListItemIcon> {index === 0 ? <ThumbUpIcon /> : <HomeIcon />} </ListItemIcon>
+                <ListItemText primary={text} />
+            </ListItem>
+        ))}  
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+        <React.Fragment key="left-drawer">
+            <MenuIcon onClick={toggleDrawer('left', true)}/>
+            <Drawer anchor='left' open={state['left']} onClose={toggleDrawer('left', false)}>
+                {list('left')}
+            </Drawer>
+        </React.Fragment>
+    </div>
+  );
+}
